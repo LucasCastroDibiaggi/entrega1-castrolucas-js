052dea7d1133c4506d6587113622ab10694bdb79
Lucas Castro <lucascastrod19@gmail.com>
2022-11-28 23:21:34 -0300

    preentrea-js-1

---
+++ main.js
@@ -0,0 +1,36 @@
+//codigo js
+
+
+
+function mensajeBienvenida(){
+    let nombre = prompt("Cual es tu nombre?");
+    alert(`Bienvenido a adidas, ${nombre}`);
+}
+
+mensajeBienvenida()
+
+let nombre  = "Lucas";
+
+let apellido = "Castro";
+
+let añoNacimiento = 2001;
+
+let edad = 21;
+
+
+Notificacion = prompt("Permite recibir notificaciones del sitio?");
+
+// IF
+
+let apto = prompt("Cual es tu edad?");
+
+if (edad > 18) {
+    alert("puedes usar esta pagina");
+
+    }else {
+        alert("no cumplis con la edad minima para usar esta pagina");
+
+    }
+
+
+
